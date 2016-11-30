0cc43e17a01528370f6dc065c4b22430e92ca1ac
Alper Altuntas <alperaltuntas@gmail.com>
2016-11-30 17:30:19 -0500

    Update genfull.py

--- genfull.py
+++ genfull.py
@@ -319,14 +319,12 @@ def main(fulldir):
 
 def usage():
     scriptName = os.path.basename(__file__)
-
     print ""
     print "Usage:"
     print " python", scriptName, "fulldomainDir"
 
 
 if __name__== "__main__":
-
     if len(sys.argv) == 2:
         main(sys.argv[1])
     else:
